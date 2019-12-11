b2f7b88d1780a8858784d32f28cc25d1e7711e16
pbaro <pbaquero@loyal.guru>
2019-12-11 17:21:57 +0100

    Update grape-kaminari.rb

--- lib/grape-kaminari.rb
+++ lib/grape-kaminari.rb
@@ -1,10 +1,5 @@
 module Grape
   module Kaminari
-    class << self
-      def post_0_9_0_grape?
-        Gem::Version.new(Grape::VERSION) > Gem::Version.new('0.9.0')
-      end
-    end
   end
 end
 
